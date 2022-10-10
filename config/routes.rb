7c55ca73ac0265792806536c28f1b81e7f44e51f
cardon <philantrope1@outlook.com>
2022-10-10 12:04:15 +0200

    add view profil and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'orders/create'
-  get 'orders/update'
-  get 'orders/destroy'
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/update'
-  get 'products/destroy'
+  #get 'orders/index'
+  #get 'orders/show'
+  #get 'orders/new'
+  #get 'orders/create'
+  #get 'orders/update'
+  #get 'orders/destroy'
+  #get 'products/index'
+  #get 'products/show'
+  #get 'products/new'
+  #get 'products/create'
+  #get 'products/update'
+  #get 'products/destroy'
+  #users
   devise_for :users
   root to: "products#index"
+  #resources :products
+  get 'profil/:id', to: 'pages#profil', as: :profil
+  resources :products, only: %i[index show new update] do
+    resources :orders, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
