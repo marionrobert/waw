858a68705652d083775d08fb432683ccb6c97ae1
cardon <philantrope1@outlook.com>
2022-10-14 16:35:18 +0200

    validation model product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
   monetize :price_cents
 
+  #belongs_to :user
+
+  validates :description, :name, :price_cents, presence: true
+  #en attente de debug error pour les images
+  #validate :images, presence: true
+
   include PgSearch::Model
     pg_search_scope :search_by_name,
       against: [ :name],
