cf1a789c530455a5214df7a8f969297dc159be81
aurelien prevost <aurelienprevost77@gmail.com>
2022-10-19 15:23:32 +0200

    menulateralleft

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+
+// menu deployant lateral gauche
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body');
+
+toggle.addEventListener('click', function() {
+  body.classList.toggle('open');
+
+})
