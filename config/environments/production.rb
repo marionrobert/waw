6bc4ef9b6c6b24d67a29a39c40f72b581beae97b
Marion Robert <mrobert148@gmail.com>
2022-11-10 16:52:50 +0100

    new updates for contactform in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,10 +69,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
     port: 587,
-    user_name: 'Mon adresse email',
+    address: 'smtp.gmail.com',
+    # domain: 'https://lysianeanniversaire.herokuapp.com',
+    user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_KEY"],
     authentication: 'plain',
     enable_starttls_auto: true
