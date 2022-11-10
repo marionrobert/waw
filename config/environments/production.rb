26e388dbcb8147f025e7dda0d343311c2ff0a377
Marion Robert <mrobert148@gmail.com>
2022-11-10 12:28:25 +0100

    udpates contact form to work on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Gmail server params for contact form
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: 'Mon adresse email',
+    password: ENV["GMAIL_KEY"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
