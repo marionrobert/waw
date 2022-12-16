c8940b389ea176a029ae874de6fbf434b760e744
aurelien prevost <aurelienprevost77@gmail.com>
2022-12-16 17:28:34 +0100

    probleme_prix_promo_superieur_a_prix_normal_possible

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,13 @@ class Product < ApplicationRecord
 
   belongs_to :subcategory
   validates :description, :name, :price_cents, :subcategory_id, presence: true
+  validates :discount_price_cents, presence: true
   delegate :category, to: :subcategory, allow_nil: true
+
+  # ne fonctionne pas
+  # before_save :check_promo
+
+
 
   #en attente de debug error pour les images
   #validate :images, presence: true
@@ -16,9 +22,15 @@ class Product < ApplicationRecord
 
 
   include PgSearch::Model
-    pg_search_scope :search_by_name,
-      against: [ :name],
-      using: {
-        tsearch: { prefix: true } # <-- now `superman batm` will return something!
-      }
+  pg_search_scope :search_by_name,
+  against: [ :name],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
+
+  # ne fonctionne pas
+  # private
+  #   def check_promo
+  #     self.discount_price_cents < self.price_cents
+  #   end
 end
