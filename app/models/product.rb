264bb91a4849aa5a578d41d4fccc086c0738c36f
Marion Robert <mrobert148@gmail.com>
2022-12-16 19:01:41 +0100

    la réduction est nécessairement moins élevée que le prix initial

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,36 +1,28 @@
 class Product < ApplicationRecord
-  monetize :price_cents
   has_many_attached :photos
   has_many :line_items, dependent: :nullify
-
   has_many :favorites
-
   belongs_to :subcategory
+
   validates :description, :name, :price_cents, :subcategory_id, presence: true
-  validates :discount_price_cents, presence: true
+
+  monetize :price_cents
+  validates :price_cents, numericality: { greater_than_or_equal_to: 1 }
+
+  validates :discount_price_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }, comparison: { less_than: :price_cents, message: ": la réduction doit être moins élevée que le prix." }
+
   delegate :category, to: :subcategory, allow_nil: true
 
-  # ne fonctionne pas
-  # before_save :check_promo
+  # en attente de debug error pour les images
+  # validate :images, presence: true
 
-
-
-  #en attente de debug error pour les images
-  #validate :images, presence: true
-
-  scope :filter_by_name, -> (name) { where('name ILIKE ?', "%#{name}%") }
-
+  scope :filter_by_name, ->(name) { where('name ILIKE ?', "%#{name}%") }
 
   include PgSearch::Model
   pg_search_scope :search_by_name,
-  against: [ :name],
-  using: {
-    tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  }
+                  against: [:name],
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 
-  # ne fonctionne pas
-  # private
-  #   def check_promo
-  #     self.discount_price_cents < self.price_cents
-  #   end
 end
