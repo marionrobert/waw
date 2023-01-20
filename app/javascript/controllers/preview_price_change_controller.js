6e45b58d5520c7882fd6f468effc3d1fc0e09563
Marion Robert <mrobert148@gmail.com>
2023-01-20 20:06:44 +0100

    deleted a console and add comments in the preview js function

--- app/javascript/controllers/preview_price_change_controller.js
+++ app/javascript/controllers/preview_price_change_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
-    console.log("Hello from the previous price change controller!")
+    // console.log("Hello from the previous price change controller!")
 
   }
 
