7c620fbc513a3095c6625929672f2d99f67bc9fc
Marion Robert <mrobert148@gmail.com>
2023-02-08 12:26:46 +0100

    working on displaying prices inf ront in a simplier way

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,7 @@ class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
   def total
-    line_items.to_a.sum(&:total)
+    (line_items.to_a.sum(&:total)).to_f
   end
 
   def add_product(product)
