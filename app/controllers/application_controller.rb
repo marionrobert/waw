0be8a5bfcc91ac7a48e1866f8a8f9ba60cf09bd0
Marion Robert <mrobert148@gmail.com>
2023-02-20 10:57:19 +0100

    create set_number_icon_cart in application controller to remove code from apliccation.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   include CurrentCart
   before_action :set_cart
-  # The callback which stores the current location must be added before you authenticate the user 
-  # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect 
+  # The callback which stores the current location must be added before you authenticate the user
+  # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect
   # before the location can be stored.
   before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
@@ -33,15 +33,26 @@ class ApplicationController < ActionController::Base
     @subcategories = Subcategory.all
   end
 
+  def set_number_icon_cart
+    if @cart.line_items.count.positive?
+      @cartnumber = 0
+      all_quantities = []
+      @cart.line_items.each do |item|
+        all_quantities << item.quantity
+      end
+      @cartcontent = all_quantities.sum
+    end
+  end
+
   private
 
   # Its important that the location is NOT stored if:
   # - The request method is not GET (non idempotent)
-  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an 
+  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
   #    infinite redirect loop.
   # - The request is an Ajax request as this can lead to very unexpected behaviour.
   def storable_location?
-    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
   end
 
   def store_user_location!
