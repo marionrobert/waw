b33c4a464afae42666bc85a793d8cf637dfe3ede
Marion Robert <mrobert148@gmail.com>
2023-03-29 18:40:26 +0200

    autocompleted name with stimulus controller and data targets

--- app/javascript/controllers/autocompleteproduct_controller.js
+++ app/javascript/controllers/autocompleteproduct_controller.js
@@ -1,37 +1,12 @@
-import { Controller } from "stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["sku", "width", "height", "orientation", "support", "name"]
+
+  connect(){
+  }
 
   nameautocomplete() {
-    console.log("connécté")
-    document.addEventListener("DOMContentLoaded", function() {
-      const skuField = document.querySelector("#product_sku");
-      const widthField = document.querySelector("#product_width");
-      const heightField = document.querySelector("#product_height");
-      const orientationField = document.querySelector("#product_orientation");
-      const supportField = document.querySelector("#product_support");
-      const nameField = document.querySelector("#product_name");
-
-      const updateNameField = () => {
-        const skuValue = skuField.value.trim();
-        const widthValue = widthField.value.trim();
-        const heightValue = heightField.value.trim();
-        const orientationValue = orientationField.value.trim();
-        const supportValue = supportField.value.trim();
-
-        if (skuValue !== "" && widthValue !== "" && heightValue !== "" && orientationValue !== "" && supportValue !== "") {
-          const newName = `${skuValue}_${widthValue}x${heightValue}_${orientationValue}_${supportValue}`;
-          nameField.value = newName;
-        } else {
-          nameField.value = "";
-        }
-      };
-
-      skuField.addEventListener("input", updateNameField);
-      widthField.addEventListener("input", updateNameField);
-      heightField.addEventListener("input", updateNameField);
-      orientationField.addEventListener("input", updateNameField);
-      supportField.addEventListener("input", updateNameField);
-    });
+    this.nameTarget.value = `${this.skuTarget.value}_${this.widthTarget.value}x${this.heightTarget.value}_${this.orientationTarget.value}_${this.supportTarget.value}`
   }
 }
