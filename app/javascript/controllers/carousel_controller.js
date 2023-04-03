c9743cf895eb779cb755dd9833e17522b2376351
aurelien prevost <aurelienprevost77@gmail.com>
2023-04-03 17:00:49 +0200

    ready2merge

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   connect() {
-    console.log("controller carousel connecte");
     window.addEventListener("load", this.initialize.bind(this));
   }
 
@@ -15,20 +14,15 @@ export default class extends Controller {
 
     this.prevButton.addEventListener("click", this.prev.bind(this));
     this.nextButton.addEventListener("click", this.next.bind(this));
-    console.log("fin de connect");
   }
 
   prev() {
-    console.log("clic prev carousel");
     this.index = this.index > 0 ? this.index - 1 : 0;
     this.carouselgroupContent.style.transform = `translateX(-${this.index * this.carouselgroupItemWidth * 0.4}px)`;
-    console.log("fin de clic prev carousel");
   }
 
   next() {
-    console.log("clic next carousel");
     this.index = this.index < this.carouselgroupContent.children.length - 1 ? this.index + 1 : this.index;
     this.carouselgroupContent.style.transform = `translateX(-${this.index * this.carouselgroupItemWidth * 0.4}px)`;
-    console.log("fin de clic next carousel");
   }
 }
