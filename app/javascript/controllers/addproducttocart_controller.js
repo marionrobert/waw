a4d52784aaca38f5d3693415384eb5a2dc504e23
Marion Robert <mrobert148@gmail.com>
2023-04-26 11:09:32 +0200

    avancées

--- app/javascript/controllers/addproducttocart_controller.js
+++ app/javascript/controllers/addproducttocart_controller.js
@@ -4,14 +4,28 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
-    console.log("boioioiol")
+    console.log("ioioioioio")
   }
 
   addproduct(event) {
     event.preventDefault();
     console.log("addproduct has been triggered")
-    const url = "/line_items"
-    const product_id = document.getElementsByClassName("button_to")[1].getAttribute("action").match(/\d+/)[0]
+    const url = "/line_items/new"
+    const product_id = document.getElementsByClassName("button_to")[1].getAttribute("action").match(/product_id=\d+/)[0]
+    console.log(product_id)
 
+    const options = {
+      method: 'POST',
+      body: product_id,
+      headers: {'Accept': 'application/json'}
+    }
+
+    fetchWithToken(url, options)
+      .then(response => response.json())
+      .then((data) => {
+        console.log("ok")
+      });
+
+  // ajouter un numéro au panier
   }
 }
