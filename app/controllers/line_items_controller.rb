3069945483216a8b074803197b7869ebf5f5c991
Marion Robert <mrobert148@gmail.com>
2023-05-01 00:14:48 +0200

    contenu du panier actualisé à l'ajout d'un produit

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -19,12 +19,13 @@ class LineItemsController < ApplicationController
     total_cart = @line_item.cart.total / 100
     @line_item.cart.line_items.each do |line_item|
       product = {
+        id: line_item.product.id,
         name: line_item.product.name,
         sku: line_item.product.sku,
         quantity: line_item.quantity,
         image_source: [cl_image_path(line_item.product.photos.first.key, secure: true)],
-        original_price: line_item.product.price_cents,
-        discount_price: line_item.product.discount_price_cents,
+        original_price: (line_item.product.price_cents / 100),
+        discount_price: (line_item.product.discount_price_cents / 100),
         discount_percent: (((line_item.product.discount_price_cents.to_f - line_item.product.price_cents.to_f) / line_item.product.price_cents.to_f) * 100).round(2).to_s.gsub(/\./, ',')
       }
       products_in_cart << product
--- app/javascript/controllers/addproducttocart_controller.js
+++ app/javascript/controllers/addproducttocart_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
   static targets = [ "contenupanier" ]
 
   connect() {
-    console.log("lll from addproducttocart")
+    console.log("ooo from addproducttocart")
   }
 
   addproduct(event) {
@@ -36,11 +36,72 @@ export default class extends Controller {
         circle_items_count.classList.remove("cartflash");
       }, 1500);
 
-      console.log(data.total_cart)
+      // console.log(data.total_cart)
+      this.contenupanierTarget.innerHTML = "";
 
       data.products.forEach(product => {
-        console.log(product);
+        // console.log(product.name)
+        let html_to_insert=`
+        <div id="lineItem${product.id}">
+          <div class="onelineitem">
+            <div class="vignette-line-item">
+              <img src=${product.image_source}>
+            </div>
+
+          <div class="line-item-central-area">
+
+            <div class="line-item-info">
+              <p class="line-item-product-name">${product.name}</p>
+              <p class="line-item-sku">REF: ${product.sku}</p>
+            </div>
+
+            <div class="quantity-zone">
+              <span class="quantity-icon" data-action="click->line-item-quantity#addOne" data-line-item-quantity-item-id-param="${product.id}">
+                <i class="fa-solid fa-plus "></i>
+              </span>
+              <span style="margin:0 5%" id="quantityItem${product.id}">
+                ${product.quantity}
+              </span>
+              <span class="quantity-icon" data-action="click->line-item-quantity#removeOne" data-line-item-quantity-item-id-param="${product.id}">
+                <i class="fa-solid fa-minus"></i>
+              </span>
+            </div>
+
+          </div>
+
+          <div class="price-zone">
+              <p class="price-off"><span id="totalOriginalPrice${product.id}">${product.original_price}</span> €</p>
+              <span class="discount-percent">-${product.discount_percent}%</span>
+              <p class="promo-price"><span id="totalPromoPrice${product.id}">${product.discount_price}</span> €</p>
+              <span style="display:none" id="totalPrice${product.id}"></span>
+          </div>
+
+          <div class="trashzone">
+            <span data-action="click->line-item-quantity#delete" data-line-item-quantity-item-id-param="${product.id}">
+              <i class="fa-solid fa-trash-can" style="color:grey"></i>
+            </span>
+          </div>
+        </div>
+
+        <div class="line-item-separator"></div>
+      </div>
+      `
+      console.log(html_to_insert)
+      this.contenupanierTarget.insertAdjacentHTML('beforeend', html_to_insert);
       });
+      let total_cart_amount = `<div class="subtotal-line">
+      <div class="subtotal-txt">
+        <p>SOUS-TOTAL TTC</p>
+      </div>
+      <div class="subtotal">
+        <p>
+          <span id="amountCart">${data.total_cart}</span> €
+        </p>
+      </div>
+    </div>`
+      console.log(total_cart_amount)
+      this.contenupanierTarget.insertAdjacentHTML('beforeend', total_cart_amount);
+
     });
   }
 }
