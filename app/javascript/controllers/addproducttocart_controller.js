9463813c531433ac837ccc427a2c70cf7fa1ba1d
Marion Robert <mrobert148@gmail.com>
2023-05-01 01:13:01 +0200

    addone removeone and delete item repaired for last product added

--- app/javascript/controllers/addproducttocart_controller.js
+++ app/javascript/controllers/addproducttocart_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
   static targets = [ "contenupanier" ]
 
   connect() {
-    console.log("ooo from addproducttocart")
+    console.log("www from addproducttocart")
   }
 
   addproduct(event) {
@@ -36,71 +36,75 @@ export default class extends Controller {
         circle_items_count.classList.remove("cartflash");
       }, 1500);
 
-      // console.log(data.total_cart)
       this.contenupanierTarget.innerHTML = "";
 
       data.products.forEach(product => {
-        // console.log(product.name)
+        console.log(product.line_item_id);
         let html_to_insert=`
-        <div id="lineItem${product.id}">
+        <div id="lineItem${product.line_item_id}">
           <div class="onelineitem">
-            <div class="vignette-line-item">
-              <img src=${product.image_source}>
-            </div>
-
+            <div class="vignette-line-item"><img src=${product.image_source}></div>
           <div class="line-item-central-area">
-
             <div class="line-item-info">
               <p class="line-item-product-name">${product.name}</p>
               <p class="line-item-sku">REF: ${product.sku}</p>
             </div>
-
             <div class="quantity-zone">
-              <span class="quantity-icon" data-action="click->line-item-quantity#addOne" data-line-item-quantity-item-id-param="${product.id}">
-                <i class="fa-solid fa-plus "></i>
+              <span class="quantity-icon" data-action="click->line-item-quantity#addOne" data-line-item-quantity-item-id-param="${product.line_item_id}">
+                <i class="fa-solid fa-plus"></i>
               </span>
-              <span style="margin:0 5%" id="quantityItem${product.id}">
-                ${product.quantity}
-              </span>
-              <span class="quantity-icon" data-action="click->line-item-quantity#removeOne" data-line-item-quantity-item-id-param="${product.id}">
+              <span style="margin:0 5%" id="quantityItem${product.line_item_id}">${product.quantity}</span>
+              <span class="quantity-icon" data-action="click->line-item-quantity#removeOne" data-line-item-quantity-item-id-param="${product.line_item_id}">
                 <i class="fa-solid fa-minus"></i>
               </span>
             </div>
+          </div>`
 
+        let html_price_zone = ""
+        if (product.discount_price === 0) {
+          html_price_zone = `<div class="price-zone">
+          <p><span id="totalPrice${product.line_item_id}">${product.original_price}</span>€</p>
+          <span style="display:none" id="totalOriginalPrice${product.line_item_id}"></span>
+          <span style="display:none" id="totalPromoPrice${product.line_item_id}"></span>
+          </div>`
+        } else {
+          html_price_zone = `<div class="price-zone">
+          <p class="price-off"><span id="totalOriginalPrice${product.line_item_id}">${product.original_price}</span> €</p>
+          <span class="discount-percent">${product.discount_percent}%</span>
+          <p class="promo-price"><span id="totalPromoPrice${product.line_item_id}">${product.discount_price}</span> €</p>
+          <span style="display:none" id="totalPrice${product.line_item_id}"></span>
+          </div>`
+        }
+        html_to_insert += html_price_zone
+
+
+        let html_final_zone = `<div class="trashzone">
+        <span data-action="click->line-item-quantity#delete" data-line-item-quantity-item-id-param="${product.line_item_id}">
+          <i class="fa-solid fa-trash-can" style="color:grey"></i>
+        </span>
+            </div>
           </div>
 
-          <div class="price-zone">
-              <p class="price-off"><span id="totalOriginalPrice${product.id}">${product.original_price}</span> €</p>
-              <span class="discount-percent">-${product.discount_percent}%</span>
-              <p class="promo-price"><span id="totalPromoPrice${product.id}">${product.discount_price}</span> €</p>
-              <span style="display:none" id="totalPrice${product.id}"></span>
-          </div>
+          <div class="line-item-separator"></div>
+        </div>`
+        html_to_insert += html_final_zone
+        this.contenupanierTarget.insertAdjacentHTML('beforeend', html_to_insert);
+      });
 
-          <div class="trashzone">
-            <span data-action="click->line-item-quantity#delete" data-line-item-quantity-item-id-param="${product.id}">
-              <i class="fa-solid fa-trash-can" style="color:grey"></i>
-            </span>
-          </div>
+      let total_cart_amount = `
+      <div class="subtotal-line">
+        <div class="subtotal-txt">
+          <p>SOUS-TOTAL TTC</p>
         </div>
-
-        <div class="line-item-separator"></div>
+        <div class="subtotal">
+          <p>
+            <span id="amountCart">${data.total_cart}</span> €
+          </p>
+        </div>
       </div>
       `
-      console.log(html_to_insert)
-      this.contenupanierTarget.insertAdjacentHTML('beforeend', html_to_insert);
-      });
-      let total_cart_amount = `<div class="subtotal-line">
-      <div class="subtotal-txt">
-        <p>SOUS-TOTAL TTC</p>
-      </div>
-      <div class="subtotal">
-        <p>
-          <span id="amountCart">${data.total_cart}</span> €
-        </p>
-      </div>
-    </div>`
-      console.log(total_cart_amount)
       this.contenupanierTarget.insertAdjacentHTML('beforeend', total_cart_amount);
+
 
     });
   }
